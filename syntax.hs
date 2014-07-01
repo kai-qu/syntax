b4ab4e3a6ab0e96292d925b0fcce7d65a69dbfe1
katherineye <kye@princeton.edu>
2014-07-01 01:37:35 -0700

    clean up code and add dickinson example

--- syntax.hs
+++ syntax.hs
@@ -2,30 +2,31 @@ import System.Environment
 import System.IO
 import System.Exit
 import Data.Char       
--- import Control.Arrow
 
--- TODO run on self
--- TODO batch-process files
-
+-- Handle command-line arguments (filenames)
 main :: IO ()
 main = do
      args <- getArgs
      case args of
        []     -> do
          name <- getProgName
-         hPutStrLn stderr $ "usage: ./" ++ name ++ " [filename(s)]"
+         hPutStrLn stderr $ "usage: ./[binary name] [filename(s)]"
          exitFailure
        _      -> do
          mapM_ readAndWrite args
 
+-- Outputs syntaxified "file.extension" as "syntax-file.extension"
 readAndWrite :: FilePath -> IO ()
 readAndWrite arg = do     
      contents <- readFile arg 
      writeFile ("syntax-" ++ arg) (syntaxify contents)
 
--- should probably do this with a parser or regex or something more sophisticated
+-- Could do this with a parser or regex or something more sophisticated
+-- Also, String is slow for large files. Text is better
 syntaxify :: String -> String
 syntaxify = map removeNonSyntax
   where removeNonSyntax char
          | isLetter char || isDigit char = ' '
          | otherwise                     = char
+
+-- Removing all the punctuation isn't quite as interesting
